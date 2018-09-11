aa689465f16124938960af7a9c0d18a0118b6ff7
Bruno Ferreira <bruno.ferreira@inovamais.pt>
2018-09-11 16:48:32 +0100

    change

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -2,11 +2,16 @@
 
 namespace myApp
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      if (args.Length > 0)
+      {
+        Console.WriteLine($"Hello {args[0]}!");
+      }
+      else
+        Console.WriteLine("Hello World!");
     }
+  }
 }
